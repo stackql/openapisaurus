0f55616a7df5f2ecad4d0a8c4fba343783f4c68b
Yuncheng <fabioyang781@gmail.com>
2023-06-03 11:24:28 +0800

    need to fix datadog

--- util/providers.ts
+++ util/providers.ts
@@ -10,7 +10,7 @@ export function updateServiceName(providerName: string, inServiceName: string, d
     }
     return outServiceName;
 }
- 
+
 function getResourceNameFromOperationId(data: any, operationId: string, debug: boolean, logger: any): string | false {
     debug ? logger.debug(`running getResourceNameFromOperationId for ${operationId}...`) : null;
     if ('opIdMap' in data) {
@@ -33,25 +33,25 @@ function getNewResourceNameFromExisting(data: any, resourceName: string, debug: 
     return false;
 }
 
-export function updateResourceName(providerName: string, service: string, inResourceName: string, operation: any, debug: boolean, logger: any): string {
-    debug ? logger.debug(`checking for resource name updates for [${inResourceName}] in [${providerName}.${service}]...`) : null;
+export function updateResourceName({ providerName, service, inResourceName, operation, debug, logger }: { providerName: string, service: string, inResourceName: string, operation: any, debug: boolean, logger: any }): string {
+    debug && logger.debug(`checking for resource name updates for [${inResourceName}] in [${providerName}.${service}]...`);
     let outResourceName = inResourceName;
     if (providerName in providers) {
-        debug ? logger.debug(`provider data found for ${providerName}`) : null;
+        debug && logger.debug(`provider data found for ${providerName}`);
         if (service in providers[providerName].resourcesMap) {
-            debug ? logger.debug(`service data found for ${providerName}.${service}`) : null;
+            debug && logger.debug(`service data found for ${providerName}.${service}`);
             // try to get new name from operationId
-            const nameFromOpIdOrNull = getResourceNameFromOperationId(providers[providerName].resourcesMap[service], operation.operationId, debug, logger); 
+            const nameFromOpIdOrNull = getResourceNameFromOperationId(providers[providerName].resourcesMap[service], operation.operationId, debug, logger);
             if (!nameFromOpIdOrNull) {
                 // try to get new name from existing name
                 const nameFromExistingOrNull = getNewResourceNameFromExisting(providers[providerName].resourcesMap[service], inResourceName, debug, logger);
                 if (nameFromExistingOrNull) {
                     outResourceName = nameFromExistingOrNull;
-                    debug ? logger.debug(`resource name changed from ${inResourceName} to ${outResourceName} via name mapping`) : null;
+                    debug && logger.debug(`resource name changed from ${inResourceName} to ${outResourceName} via name mapping`);
                 }
             } else {
                 outResourceName = nameFromOpIdOrNull;
-                debug ? logger.debug(`resource name changed from ${inResourceName} to ${outResourceName} via operationId`) : null;
+                debug && logger.debug(`resource name changed from ${inResourceName} to ${outResourceName} via operationId`);
             }
         }
     }
@@ -59,11 +59,11 @@ export function updateResourceName(providerName: string, service: string, inReso
     return outResourceName;
 }
 
-export function getObjectKeyforProvider(providerName: string, service: string, resource: string, operationId: string, debug: boolean) : string | false {
+export function getObjectKeyforProvider(providerName: string, service: string, resource: string, operationId: string, debug: boolean): string | false {
 
     if (providerName in providers) {
         const providerObjectKeysAndSqlVerbs = providers[providerName].objectKeysAndSqlVerbs;
-        
+
         let objectKey = '_defaultObjectKey';
 
         if ('_defaultObjectKey' in providerObjectKeysAndSqlVerbs) {
@@ -73,7 +73,7 @@ export function getObjectKeyforProvider(providerName: string, service: string, r
         if (service in providerObjectKeysAndSqlVerbs) {
             if (resource in providerObjectKeysAndSqlVerbs[service]) {
                 if (operationId in providerObjectKeysAndSqlVerbs[service][resource]) {
-                    if ('objectKey' in providerObjectKeysAndSqlVerbs[service][resource][operationId]){
+                    if ('objectKey' in providerObjectKeysAndSqlVerbs[service][resource][operationId]) {
                         objectKey = providerObjectKeysAndSqlVerbs[service][resource][operationId]['objectKey'];
                     }
                 }
@@ -85,7 +85,7 @@ export function getObjectKeyforProvider(providerName: string, service: string, r
         } else {
             return objectKey;
         }
-        
+
     }
     return false;
 }
@@ -95,7 +95,7 @@ export function getSqlVerbforProvider(operationId: string, verbKey: string, prov
         if (service in providers[providerName].objectKeysAndSqlVerbs) {
             if (resource in providers[providerName].objectKeysAndSqlVerbs[service]) {
                 if (operationId in providers[providerName].objectKeysAndSqlVerbs[service][resource]) {
-                    if ('sqlVerb' in providers[providerName].objectKeysAndSqlVerbs[service][resource][operationId]){
+                    if ('sqlVerb' in providers[providerName].objectKeysAndSqlVerbs[service][resource][operationId]) {
                         return providers[providerName].objectKeysAndSqlVerbs[service][resource][operationId]['sqlVerb'];
                     }
                 }
