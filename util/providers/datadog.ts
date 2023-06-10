01681247a0c509e2a52eda7ea01ca46a57750c0b
Yuncheng <fabioyang781@gmail.com>
2023-06-10 15:04:40 +0800

    fixed metrics resource

---
+++ util/providers/datadog.ts
@@ -0,0 +1,8 @@
+export const resourcesMap = {
+    metrics: {
+        opIdMap: {
+            api_v1_metrics_get: 'v1_metrics',
+            api_v2_metrics_get: 'v2_metrics',
+        },
+    },
+};
