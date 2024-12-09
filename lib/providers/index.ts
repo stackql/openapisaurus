fac95c9ae0a20a0778e79dadcd82c9584fe4a250
Jeffrey Aven <javen@avensolutions.com>
2024-12-10 08:30:18 +1100

    updates

--- lib/providers/index.ts
+++ lib/providers/index.ts
@@ -2,3 +2,4 @@ export * as openai from './openai.ts';
 export * as confluent from './confluent.ts';
 export * as entra from './entra.ts';
 export * as digitalocean from './digitalocean.ts';
+export * as linode from './linode.ts';
